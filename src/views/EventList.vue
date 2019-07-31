13cc7da24b76ce60cc1cc0431ef5d5dfcd386936
Jayson Potter <jpotter@topiatechnology.com>
2019-07-31 16:34:59 -0700

    Lessons 1-5 from Vue Mastery

---
+++ src/views/EventList.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+    <h1>Events Listing</h1>
+    <router-link :to="{name: 'event-show', params: { id: '1'}}">Event #1</router-link>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "EventList"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/EventShow.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Event # {{ id }}</h1>
+</template>
+
+<script>
+    export default {
+        name : "EventShow",
+        props: ['id']
+    }
+</script>
+
+<style scoped>
+
+</style>
