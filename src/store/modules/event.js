edfb471cfd9b6e08eba58f3de4ba5bf8306a7d9d
Jayson Potter <jpotter@topiatechnology.com>
2019-08-02 15:52:03 -0700

    Mid Part 6 - 8:00

---
+++ src/store/modules/event.js
@@ -0,0 +1,79 @@
+import EventService from "@/services/EventService.js";
+
+export const state = {
+    events     : [],
+    eventsTotal: 0,
+    event      : {}
+};
+
+export const mutations = {
+    ADD_EVENT(state, event) {
+        state.events.push(event);
+    },
+    SET_EVENTS(state, events) {
+        state.events = events;
+    },
+    SET_EVENTS_TOTAL(state, total) {
+        state.eventsTotal = total;
+    },
+    SET_EVENT(state, event) {
+        state.event = event;
+    }
+
+};
+
+export const actions = {
+    createEvent({commit}, event) {
+        return EventService.postEvent(event)
+            .then(() => {
+                commit('ADD_EVENT', event);
+            });
+    },
+    fetchEvents({commit}, {perPage, page}) {
+        EventService.getEvents(perPage, page)
+            .then(response => {
+                //console.log('total events are ' + response.headers['x-total-count']);
+                commit('SET_EVENTS_TOTAL', response.headers['x-total-count']);
+                commit('SET_EVENTS', response.data);
+            })
+            .catch(error => {
+                console.log('There was an error ' + error.response);
+            });
+    },
+    fetchEvent({commit, getters}, id) {
+        var event = getters.getEventByID(id);
+
+        if (event) {
+            commit('SET_EVENT', event);
+        } else {
+            EventService.getEvent(id)
+                .then(response => {
+                    commit('SET_EVENT', response.data);
+                })
+                .catch(error => {
+                    console.log('There was an error getting event ' + error.response);
+                })
+        }
+    }
+
+};
+
+export const getters = {
+    categoryLength  : state => {
+        return state.categories.length;
+    },
+    doneTodos       : state => {
+        return state.todos.filter(todo => todo.done);
+    },
+    activeTodosCount: (state) => {
+        return state.todos.filter(todo => !todo.done);
+    },
+    getEventByID    : state => id => {
+        return state.events.find(event => event.id === id);
+    }
+
+    // activeTodosCount: (state, getters) => {
+    //   return state.todos.length - getters.doneTodos.length;
+    // }
+
+};
